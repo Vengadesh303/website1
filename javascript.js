b646b8aa27895e1b8454ab8f1e9433f86b912a28
Vengadesh303 <Vengadeshwarankdp@gmail.com>
2024-12-15 23:31:44 +0530

    webpage

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function home(){
+    console.log("hai from home");
+}
+function about(){
+    console.log("hai from about");
+}
+function service(){
+    console.log("hai from service");
+}
+function design(){
+    console.log("hai from design");
+}
+function contact(){
+    console.log("hai from contact");
+}
